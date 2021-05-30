df630d1acad6936e319ac964030f75e04ad3367c
Mahdi Mostafavi <mmostafavi247@gamil.com>
2021-05-30 07:11:26 +0430

    Add new Structure and logging features

--- src/Protocol2.ts
+++ src/Protocol2.ts
@@ -5,39 +5,25 @@ import NetworkLayer from './Layers/NetworkLayer'
 import PhysicalLayer from './Layers/PhysicalLayer'
 import {frame} from './Interfaces'
 
-const SenderNetworkLayer = new NetworkLayer()
-const SenderPhysicalLayer = new PhysicalLayer()
-const ReceiverNetworkLayer = new NetworkLayer()
-const ReceiverPhysicalLayer = new PhysicalLayer()
+
 const event = new EventEmitter()
-
-
-
-const sender_info = {
-  sender_id: "sender_1",
-  network: "X-network",
-  ip: "1.2.3.4"
-}
-
-// loading the data to network layer of the sender
-let dataArr: Array<string> = process.env.data!.split("")
-  while (dataArr.length > 0) {
-    let frameToSend = {data: "",info: {sender_id:"", network: "", ip: ""}};
-    if (dataArr.length < 8) {
-      frameToSend.data = dataArr.splice(0).join("")
-      frameToSend.info = sender_info
-    } else {
-      frameToSend.data = dataArr.splice(0,8).join("");
-      frameToSend.info = sender_info
-    }
-
-    SenderNetworkLayer.to_network_layer(frameToSend)
-  }
-
 
 export default class Protocol2 {
   private process
-  constructor(process: any){
+  SenderNetworkLayer: any 
+  SenderPhysicalLayer: any 
+  ReceiverNetworkLayer: any 
+  ReceiverPhysicalLayer: any
+  delay_in_channel: number 
+  frameCounter: number
+  constructor(process: any, data: string, delay_in_channel: number){
+    this.SenderNetworkLayer = new NetworkLayer()
+    this.SenderPhysicalLayer = new PhysicalLayer()
+    this.ReceiverNetworkLayer = new NetworkLayer()
+    this.ReceiverPhysicalLayer = new PhysicalLayer()
+
+    this.delay_in_channel = delay_in_channel
+    this.frameCounter = 1
     this.process = process
     // defining receive_frame event
     event.on("receive_frame", () => {
@@ -48,29 +34,55 @@ export default class Protocol2 {
     event.on("send_frame", () => {
       this.sender()
     })
+
+    const sender_info = {
+      sender_id: "sender_1",
+      network: "X-network",
+      ip: "1.2.3.4"
+    }
+    
+    // loading the data to network layer of the sender
+    let dataArr: Array<string> | [] = data.split("")
+      while (dataArr.length > 0) {
+        let frameToSend = {data: "",info: {sender_id:"", network: "", ip: ""}};
+        if (dataArr.length < 8) {
+          frameToSend.data = dataArr.splice(0).join("")
+          frameToSend.info = sender_info
+        } else {
+          frameToSend.data = dataArr.splice(0,8).join("");
+          frameToSend.info = sender_info
+        }
+    
+        this.SenderNetworkLayer.to_network_layer(frameToSend)
+      }
   }
 
   private sender = () => {
     // adding meta info to the frame
-    const frameToSend: frame |undefined = SenderNetworkLayer.from_network_layer()
+    const frameToSend: frame |undefined = this.SenderNetworkLayer.from_network_layer()
     
     // sending the frame to second node in network
-    SenderPhysicalLayer.to_physical_layer(frameToSend)
-    ReceiverPhysicalLayer.to_physical_layer(SenderPhysicalLayer.send())
-    event.emit("receive_frame")
+    this.SenderPhysicalLayer.to_physical_layer(frameToSend)
+    this.ReceiverPhysicalLayer.to_physical_layer(this.SenderPhysicalLayer.send())
+    setTimeout(() => {
+      console.log(`Sender ==> The frame ${frameToSend?.data} with id of ${this.frameCounter} is sent. waiting for ACK`);
+      event.emit("receive_frame")
+    }, Number(this.delay_in_channel)/2)
   }
   
   private receiver = () => {
-    const frameToProcess = ReceiverPhysicalLayer.from_physical_layer();
-    ReceiverNetworkLayer.to_network_layer(frameToProcess)
+    const frameToProcess = this.ReceiverPhysicalLayer.from_physical_layer();
+    this.ReceiverNetworkLayer.to_network_layer(frameToProcess)
   
     if (frameToProcess?.data === undefined){
+      console.log("receiver ==> There isn't any incoming frame. Connection ends");
       this.process.exit()
     }
     setTimeout(() => {
-      console.log(`ACK for frame ${frameToProcess?.data}. ready to receive the next frame`);
+      console.log(`receiver ==> ACK for frame ${frameToProcess?.data} with id of ${this.frameCounter}. waiting for the frame with id of ${this.frameCounter + 1}`);
+      this.frameCounter++
       event.emit("send_frame")
-    }, Number(this.process.env.ping))
+    }, Number(this.delay_in_channel) / 2)
     
   }
 
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 import Protocol2 from './Protocol2'
+const fs = require('fs')
 
-const protocol2 = new Protocol2(process)
-protocol2.start()
+fs.readFile('./src/input.json', 'utf8' , (err: any, data: any) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  const {input: bitStream, delay_in_channel} = JSON.parse(data)
+  
+
+  const protocol2 = new Protocol2(process, bitStream, delay_in_channel)
+  protocol2.start()
+
+})
+
+
